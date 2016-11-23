ce3bbf4d8b2229a63a1f2ed557dd5044615789aa
Eric Fennis <eric.fennis@gmail.com>
2016-11-23 15:55:06 +0100

    add first controller with database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Map::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->name,
+		'floor' => $faker->randomNumber(3),
+		'image' => $faker->name
+	];
+});
