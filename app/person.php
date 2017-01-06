abf9ecb9a2855d3639d14367777ac4a7c11033bd
Eric Fennis <eric.fennis@gmail.com>
2017-01-06 19:52:15 +0100

    app/person fix

--- app/person.php
+++ app/person.php
@@ -16,8 +16,7 @@ class Person extends Model {
         \Request::input('firstname') and $query->where('firstname','like','%'.\Request::input('firstname').'%');
         \Request::input('surname') and $query->where('surname','like','%'.\Request::input('surname').'%');
         \Request::input('profilepicture') and $query->where('profilepicture','like','%'.\Request::input('profilepicture').'%');
-        \Request::input('company_id') and $query->where('company_id',\Request::input('company_id'));
-        \Request::input('telephone') and $query->where('telephone','like','%'.\Request::input('telephone').'%');
+        \Request::input('telephone') and $query->where('telephone',\Request::input('telephone'));
         \Request::input('email') and $query->where('email','like','%'.\Request::input('email').'%');
         \Request::input('website') and $query->where('website','like','%'.\Request::input('website').'%');
         \Request::input('status') and $query->where('status',\Request::input('status'));
@@ -36,11 +35,10 @@ class Person extends Model {
         $rules = [
             'firstname' => 'required|string|max:64',
             'surname' => 'required|string|max:64',
-            'profilepicture' => 'required|string|max:255',
-            'company_id' => 'required|integer',
-            'telephone' => 'required|string|max:255',
+            'profilepicture' => 'required|string|max:64',
+            'telephone' => 'required|integer',
             'email' => 'required|string|max:64|email',
-            'website' => 'required|string|max:255',
+            'website' => 'required|string|max:64',
             'status' => 'required|integer',
         ];
 
